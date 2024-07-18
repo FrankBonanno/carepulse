6b958baf18fe3f4896134ffa965d05e160cd0cce
FrankBonanno <fb0023@mix.wvu.edu>
2024-07-18 09:06:53 -0400

    appwrite setup

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -9,6 +9,7 @@ import SubmitButton from '@/components/SubmitButton';
 import { useState } from 'react';
 import { UserFormValidation } from '@/lib/validation';
 import { useRouter } from 'next/navigation';
+import { createUser } from '@/lib/actions/patient.actions';
 
 export enum FormFieldType {
   INPUT = 'input',
@@ -38,12 +39,14 @@ export const PatientForm = () => {
     setIsLoading(true);
 
     try {
-      // const userData = { name, email, phone };
-      // const user = await createUser(userData);
-      // if (user) router.push(`/patients/${user.$id}/register`)
+      const userData = { name, email, phone };
+      const user = await createUser(userData);
+      if (user) router.push(`/patients/${user.$id}/register`);
     } catch (error) {
       console.log(error);
     }
+
+    setIsLoading(false);
   }
 
   return (
